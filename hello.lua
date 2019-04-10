494714326fa1d109798a8cf98b1242623701eb80
Mirza Abazovic <mirza.abazovic@gmail.com>
2019-04-10 09:21:32 +0200

    first commit

--- hello.lua
+++ hello.lua
@@ -1,6 +1,6 @@
 -- hello world
 print("hello world")
---
+
 greeting = "hi"
 print(greeting)
 
@@ -19,6 +19,8 @@ print(undefined_variable) -- will print: nil
 uinstanciated_variable = nil
 print(uinstanciated_variable) -- will print: nil
 
+print("**************************************************************************************************************")
+-- Functions
 -- Declare the function, takes two arguments
 function AddAndPrint(x, y)
     local result = x + y;
@@ -26,7 +28,7 @@ function AddAndPrint(x, y)
   end
   
 -- Call the function
-  AddAndPrint(2, 3)
+AddAndPrint(2, 3)
 -- Call the function a few times
 AddAndPrint(2, 3, 7) -- Will print 2+3=5
 AddAndPrint(4, 5, 8, 9, 10) -- Will print 4+5=9
@@ -40,7 +42,9 @@ AddAndPrint(6, 7, 11, 12, 14) -- Will print 6+7=13
 --         [C]: in ?
 
 
--- Lua has a unique feature that many traditional languages don't, multiple return values. This feature allows one function to return multiple values. To return multiple values, assign the result of the function to a list of variables separated by commas.
+-- Lua has a unique feature that many traditional languages don't, multiple return values. 
+-- This feature allows one function to return multiple values. 
+-- To return multiple values, assign the result of the function to a list of variables separated by commas.
 -- For example, you could write a function that takes a number for an argument and returns both the squared and cubed values of that number:
 function SquareAndCube(x)
     squared = x * x
@@ -54,8 +58,8 @@ print ("Squared: " .. s) -- will print: Squared: 4
 print ("Cubed: " .. c) -- will print: Cubed: 8
 
 print("**************************************************************************************************************")
---The only data structure provided by Lua is the table. 
---The table is one of the built-in data types Lua provides.
---Tables are powerful enough to implement other data structures, such as lists, queues, or stacks.
---Lua is not object-oriented; the language does not have support for objects. 
---However, using tables and meta-tables, an object system can be implemented from the ground up.
+-- The only data structure provided by Lua is the table. 
+-- The table is one of the built-in data types Lua provides.
+-- Tables are powerful enough to implement other data structures, such as lists, queues, or stacks.
+-- Lua is not object-oriented; the language does not have support for objects. 
+-- However, using tables and meta-tables, an object system can be implemented from the ground up.
